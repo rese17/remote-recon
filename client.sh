f6c9cd50782818204348b077d40fab4b90ad819f
rese <ahmedrese12@gmail.com>
2022-10-04 13:46:25 +0200

    first commit

---
+++ client.sh
@@ -0,0 +1,156 @@
+#!/bin/bash
+
+
+
+gen_random_id(){
+		echo $(hexdump -vn16 -e'4/4 "%08X" 1 "\n"' /dev/urandom)
+}
+get_status(){
+		if [ ! -f .client_status ]; then echo "idle" > .client_status; fi
+		cat .client_status
+}
+
+set_status () {
+		if [ ! -f .client_status ]; then touch .client_status; fi
+		echo $1 > .client_status
+}
+
+
+# TODO: add more debugging code functions
+run_debug (){
+		
+		echo "DEBUG" 
+		echo $resp
+		echo "server response: $resp"
+		status="node status: $(get_status) "
+		echo $status
+		
+		if [ ! -z "$cmd" ]
+		then
+				echo -e "command:\n $(cat <<<$cmd)"
+		fi
+}
+
+
+set_script () {
+		if [ ! -f .cmd_script ]
+		then
+				touch .cmd_script
+				chmod +x .cmd_script
+		fi
+
+		echo "ngrok_url=\" $ngrok_url\"" > .cmd_script
+		cat <<<"$cmd" >> .cmd_script
+		
+}
+
+
+# shared varaibles 
+STATUS=$(get_status )
+
+get_ngrok_url (){
+		# how the freaking fuck should i dget that freaking url 
+		# curl "localhost"
+
+		url=$`curl -s --location --request POST "https://data.mongodb-api.com/app/data-qwghm/endpoint/data/v1/action/findOne"  \
+							 --header "Content-Type: application/json" \
+							 --header "Access-Control-Request-Headers: \*" \
+							 --header "api-key: llHw9CnMFOKEBLS4r5oJxrp41bexNZrpKmHD0prMZshCGykhgMyDdzDjK3MrjJ0p" \
+							 --data-raw \
+							 "{\"collection\":\"config\", \"database\":\"recon-config\", \"dataSource\":\"recon-config\", \"filter\":{\"key\":\"1\"}}"`
+		echo ${url:1} | jq -r '.document.url'
+}
+
+
+
+ngrok_url=$(get_ngrok_url)
+sync_server(){
+		# syncs the node with the server 
+		ngrok_url=$(get_ngrok_url)
+		while [ ! -z "a" ]
+		do
+				# echo "syncing..."
+				resp=$(curl -s --request POST --data '{"host":"colab","status"'":\"$(get_status)\", \"id\":\"$1\"}" "$ngrok_url/sync/");
+				# handles the reponse and run the required functions
+				parse_response 
+				# run the cmd 
+				cmd="$(echo $resp | jq 'if .task then .task else empty end' | jq -r '.cmd?')"
+				if [ ! -z "$cmd" ]
+				then
+						# echo "here"
+						set_script $cmd 
+						cat <<<$cmd
+						run_command 
+				fi
+				sleep 4
+		done 
+
+}
+
+run_command(){
+		# runs the command coming from the server
+		set_status "busy"
+		run_cmd &
+		kill -CONT $!
+}
+
+# check_status(){
+# 		# checks the status of the server by
+# 		# testing if the run_command function is
+# 		# running or not 
+
+# 		# MAYBE: use a variable that is set once the
+# 		#      : the function enters and then reset 
+
+# }
+
+run_cmd (){
+		./.cmd_script
+		set_status "idle"
+}
+sid=$(gen_random_id)
+echo $sid
+sync_server $sid 
+
+kill_task () { ; }
+shutdown_program () {; }
+
+handle_signal(){
+		# if signal is stop kill the running process
+		case "$signal" in
+				"kill")
+						# handle kill
+						kill_task
+				;;
+				"wait")
+						# handle wait
+						set_status "wait"
+				;;
+				"shutdown")
+						# handle shutdown
+						shutdown_program
+				;;
+				"resume")
+						# handle resume
+						set_status "idle"
+				;;
+				esac
+				
+}
+
+parse_response (){
+		# check if there is a debugging in the response
+		debug=$(echo $resp | jq -r 'if .config?.debug? then "debug" else empty end' )
+		# check if debugging and run debugging code 
+		if [ ! -z $debug ]
+		then
+				run_debug
+		fi
+		# check for any signals from the server 
+		signal=$(echo $resp | jq -r 'if.config?.signal')
+		handle_signal 
+		# check if 
+}
+
+
+
---
+++ node_init.sh
@@ -0,0 +1,81 @@
+
+# make directory for binaries
+# use a file to check if the init shit is finished 
+mkdir content
+cd content
+
+
+
+# download stuff inside teh directory 
+wget --quiet https://github.com/OWASP/Amass/releases/download/v3.19.3/amass_linux_amd64.zip -O amass.zip
+wget --quiet https://github.com/projectdiscovery/nuclei/releases/download/v2.7.7/nuclei_2.7.7_linux_amd64.zip -O nuclei.zip 
+wget --quiet https://github.com/projectdiscovery/naabu/releases/download/v2.1.0/naabu_2.1.0_linux_amd64.zip -O naabu.zip
+wget --quiet https://github.com/projectdiscovery/httpx/releases/download/v1.2.4/httpx_1.2.4_linux_amd64.zip -O httpx.zip
+wget --quiet https://github.com/lc/gau/releases/download/v2.1.2/gau_2.1.2_linux_amd64.tar.gz -O gau.tar.gz
+wget --quiet https://github.com/jaeles-project/gospider/releases/download/v1.1.6/gospider_v1.1.6_linux_x86_64.zip -O gospider.zip
+wget --quiet https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 -O jq; chmod +x jq 
+wget --quiet https://github.com/tomnomnom/unfurl/releases/download/v0.4.3/unfurl-linux-amd64-0.4.3.tgz -O unfurl.tgz
+wget --quiet https://pixeldrain.com/api/file/GibrBBs9 -O cariddi; chmod +x cariddi 
+wget --quiet https://pixeldrain.com/api/file/PhBN89Sj -O domxss; chmod +x domxss 
+wget --quiet https://github.com/ffuf/ffuf/releases/download/v1.5.0/ffuf_1.5.0_linux_amd64.tar.gz -O fuff.tar.gz
+
+# unpack stuff 
+unzip -o amass.zip; rm amass.zip
+unzip -o nuclei.zip ; rm nuclei.zip
+unzip -o naabu.zip ; rm naabu.zip
+unzip -o httpx.zip ; rm httpx.zip
+tar -xf gau.tar.gz ; rm gau.tar.gz
+tar -xzf unfurl.tgz; rm unfurl.tgz
+tar -xzf fuff.tar.gz; rm fuff.tar.gz; chmod +x fuff;  
+unzip -o gospider.zip; rm gospider.zip
+
+# get all executables from inner directories 
+mkdir bin
+find . -type f -not -path "./bin/*"  -executable -exec mv {} ./bin/ \;
+find . -not -path "./bin*" -exec rm -rf {} \;
+
+
+# get python git packages 
+mkdir github
+git clone https://github.com/s0md3v/XSStrike github/ 
+echo -e "#!/bin/dash\npython3 \n python github/XSStrike/xsstrike.py $@" > bin/xsstrike; chmod +x bin/xsstrike;
+git clone https://github.com/m4ll0k/Atlas.git github/atlas
+echo -e "#!/bin/dash\npython3 \n python github/atlas/atlas.py $@" > bin/atlas; chmod +x bin/atlas;
+echo "there in this shit "
+
+
+# fuzzing and payloads 
+mkdir payloads 
+mkdir payloads/vuln-payloads
+cd payloads; git clone https://github.com/swisskyrepo/PayloadsAllTheThings;
+
+# get python pip packages  
+pip install wfuzz
+pip install dirsearch
+pip install sqlmap
+pip install wafw00f
+pip install xsstrike
+pip install photon
+pip install arjun
+pip install s3recon
+
+
+## finally link everything in the /notebooks/bin/ directory to the /usr/local/bin
+ln -s -f $(pwd)/bin/* /usr/local/bin/
+
+echo "init finished" > .init_finished
+
+
+
+# pastebin syncing 
+api_user_key=$(curl -s -X POST -d 'api_dev_key=hizd6zKuf1leXglo1nYvDzG_u0_zsf8U' -d 'api_user_name=ntrysii' -d 'api_user_password=QntsXhDLdNbYf^wbrUsq5' "https://pastebin.com/api/api_login.php")
+curl -s -X POST -d 'api_dev_key=hizd6zKuf1leXglo1nYvDzG_u0_zsf8U' -d "api_user_key=$api_user_key"  -d 'api_paste_key=4ZU4ZKja' -d 'api_option=show_paste'  "https://pastebin.com/api/api_post.php" > client.sh
+chmod +x client.sh 
+sed -i 's/\r//' client.sh
+
+pastebin_create_file(){;}
+pastebin_delete_file(){;}
+pastebin_update_file (){;}
+pastebin_get_file(){;}
+
+download_scripts(){;}
