064210c84d8c5a3fed4baff0aa1ed5cc654e3ee9
rese <ahmedrese12@gmail.com>
2022-10-21 00:34:34 +0200

    added some configurations to the local_tools.sh file

--- scripts/local_tools.sh
+++ scripts/local_tools.sh
@@ -8,11 +8,14 @@
 # }
 
 # TEST: testing without sending to the program 
+
 make_task(){
 		task=$(python3 /home/rese/bug-hunting/command_to_json.py --cmd "$@")
 		echo $task
 		curl --request POST --data "$task" "http://localhost:8020/tasks/add/"
 }
+
+
 
 # * check http servers
 get_http_servers () {
@@ -20,7 +23,7 @@ get_http_servers () {
 							-I {{files1}} \
 							-o {{files2}} \
 							---files1 $(ls -1 *domain.txt) \
-							---files2 $(ls -1 *domain.txt | sed -n 's/\-domain.txt$/-http.json/p')
+							---files2 $(ls -1 *domain.txt | sed -n 's/\-domain.txt$/-http.json/p') \
 }
 
 rhttpx () {make_task gethttp $@}
@@ -28,19 +31,20 @@ rhttpx () {make_task gethttp $@}
 # * spider http sites
 
 rspider() {
+		# TODO: create the http.txt file
 		make_task gaufile \
 							-i {{files1}} \
 							-o {{files2}} \
-							---files1 *-http.json \
-							---files2 $(echo *-http.json | sed -n 's/-http\.json/-gau-urls.json/p') \
-							---local false
+							---files1 *-http.txt \
+							---files2 $(echo *-http.txt | sed -n 's/-http\.txt/-gau-urls.json/p') \
+							# ---remote-only 
 							
 		make_task spidersite \
-							-i {{files1}} \
+							-I {{files1}} \
 							-o {{files2}} \
-							---files1 *-http.json \
-							---files2 $(ls -1 *-http.json | sed -n 's/-http\.json/-gospider-urls.json/p') \
-							---local false
+							---files1 *-http.txt \
+							---files2 $(ls -1 *-http.txt | sed -n 's/-http\.txt/-gospider-urls.json/p') \
+							# ---remote-only
 }
 
 # rgospider (){ make_task spidersite $@ }
@@ -48,14 +52,28 @@ rspider() {
 # * check secrets, domains in js files and so on
 
 get_secrets (){
-		subdomainizer $@ 
+		echo $@
+		make_task subdomainizer \
+							-i {{list1}} \
+							-o {{files2}} \
+							---list1 *-http.txt \
+							---files2 $(echo *-http.txt | sed -n 's/-http\.json/-subdominizer.json/p') \
+							---remote-only \
+							---active-nodes 10
+		
+		
 		# TODO: more secrets
 }
 
 
 # * check technologies 
 get_technologies () {
-		wapp $@
+							-i {{list1}} \
+							-o {{files2}} \
+							---list1 *-http.txt \
+							---files2 $(echo *-http.txt | sed -n 's/-http\.json/-subdominizer.json/p') \
+							---local false
+		wapp   
 }
 
 # * others
@@ -64,6 +82,7 @@ get_technologies () {
 # ** vulnerabilities 
 # *** sqlmap
 
-
-get_http_servers
  
+
+rspider
+
