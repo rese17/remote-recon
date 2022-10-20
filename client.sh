2dcf5ea53042da9ade013caa5f5cce36daeafcea
rese <ahmedrese12@gmail.com>
2022-10-20 18:36:02 +0200

    testing sourcing the files from an absolute path

--- client.sh
+++ client.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# the programs to run are all stored in here
+# TEST:
+# save the current dir and go back again
+source /home/rese/programming-projects/bash/remote-node/scripts/tools.sh
 
 # use the content/bin in the path 
 PATH="~/content/bin/:$PATH"
