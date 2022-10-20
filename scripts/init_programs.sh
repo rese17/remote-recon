995dce85f691405586c32893119cfd9c65c6c00c
rese <ahmedrese12@gmail.com>
2022-10-21 00:34:02 +0200

    moving the commands from the init_programs.sh to tools.sh

--- scripts/init_programs.sh
+++ scripts/init_programs.sh
@@ -15,15 +15,7 @@ fi
 
 
 # *** subdominizer
-run_subdominizier(){
-		# install 
-		
-		python3 SubDomainizer/SubDomainizer.py -u $1 -o .subdom_output -cop .subdom_cloud -gop .subdom_github -k -gt "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" -g > /dev/null
-		# files to json
-		python3 entry_to_json.py .subdom_output "domain" $1
-		python3 entry_to_json.py .subdom_cloud "cloud" $1
-		python3 entry_to_json.py .subdom_github "github" $1
-}
+
 
 # *** 
 
