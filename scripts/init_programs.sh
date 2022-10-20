5885e801599b6a4c5bbd50d4f02e32a93faaa527
rese <ahmedrese12@gmail.com>
2022-10-21 00:33:30 +0200

    using the new ROOT_DIR/scripts as the default dir for new scripts

--- scripts/init_programs.sh
+++ scripts/init_programs.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-# * git packages download 
-if [ ! -d SubDomainizer ]; then git clone https://github.com/nsonaniya2010/SubDomainizer; fi
+
+# NOTE: use the root directory for all newly added scripts
+if [ ! -d $ROOT_DIR/scripts/SubDomainizer ]; then
+		git clone https://github.com/nsonaniya2010/SubDomainizer $ROOT_DIR/scripts/SubDomainizer/;
+		python3 -m pip install -r $ROOT_DIR/scripts/SubDomainizer/requirements.txt
+fi
 
 
 
